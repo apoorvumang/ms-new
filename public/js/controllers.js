c6e217ccb610eef0e2be23f9eb452710099da4fa
Apoorv Umang Saxena <apoorv.saxena@flipkart.com>
2015-03-24 15:37:52 +0530

    changed to angular 1.3.15 in order to fix date input bug. had to make additional changes due to angular version change

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -25,151 +25,147 @@ function isEmpty(obj) {
 
 /* Controllers */
 
-function AppCtrl($scope, $http) {
-    $http({method: 'GET', url: '/api/name'}).
-        success(function (data, status, headers, config) {
-            $scope.name = data.name;
-        }).
-        error(function (data, status, headers, config) {
-            $scope.name = 'Error!';
-        });
-}
-
-function MyCtrl1($scope) {
-    $scope.message = "asd";
-    console.log($scope.message);
-}
-MyCtrl1.$inject = ['$scope'];
-
-function MyCtrl2() {
-}
-MyCtrl2.$inject = [];
-
-function AddPatientCtrl() {
-}
-AddPatientCtrl.$inject = [];
-
-function EditPatientCtrl($scope, $http, $routeParams) {
-    $scope.editPatient = false;
-    $scope.toggleEditing = function () {
-        $scope.editPatient = true;
-    };
-    $http({method: 'GET', url: '/api/patientAndSiblings/' + $routeParams['patientID']}).
-        success(function (data, status, headers, config) {
-            $scope.patient = data;
-        }).
-        error(function (data, status, headers, config) {
-            $scope.patient = {};
-        });
-}
-EditPatientCtrl.$inject = ['$scope', '$http', '$routeParams'];
-
-function SearchPatientCtrl($scope, $http) {
-    $scope.patient = {};
-    $scope.patientList = [];
-    $scope.fieldList = [
-        {name: "id", displayName: "ID"},
-        {name: "name", displayName: "Name"},
-        {name: "dob", displayName: "DOB"},
-        {name: "sex", displayName: "Sex"},
-        {name: "phone", displayName: "Phone"}
-    ];
-    $scope.orderByField = 'id';
-    $scope.reverseSort = false;
-    $scope.defaultSMSMessage = "Dear parent,\nWishing your child %name% a very Happy Birthday!\nDr. Mahima\n981129950";
-    $scope.smsMessage = $scope.defaultSMSMessage;
-    $scope.smsSuccess = false;
-    $scope.numSMSSuccess = 0;
-    $scope.smsFailed = false;
-
-    var processData = function (pList) {
-        for (var i = 0; i < pList.length; i++) {
-            pList[i].sms = false;
-        }
-        return pList;
-    };
-
-    $scope.searchByID = function (patientId) {
-        $http({method: 'GET', url: '/api/patient/' + patientId}).
+angular.module('myApp.controllers', []).
+    controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
+        $http({method: 'GET', url: '/api/name'}).
             success(function (data, status, headers, config) {
-                if (!isEmpty(data))
-                    $scope.patientList = processData([data]);
+                $scope.name = data.name;
             }).
             error(function (data, status, headers, config) {
-                $scope.patientList = [];
+                $scope.name = 'Error!';
             });
-    };
+    }]).
+    controller('MyCtrl1', ['$scope', function($scope) {
+        $scope.message = "asd";
+        console.log($scope.message);
 
-    $scope.searchByDOB = function (dob) {
-        $http({method: 'GET', url: '/api/patient/dob/' + dob}).
+    }]).
+    controller('AddPatientCtrl', [function() {
+
+    }]).
+    controller('EditPatientCtrl', ['$scope', '$http', '$routeParams', function($scope, $http, $routeParams) {
+        $scope.editPatient = false;
+        $scope.toggleEditing = function () {
+            $scope.editPatient = true;
+        };
+        $scope.example = {
+            value: new Date(2013, 9, 22)
+        };
+        $http({method: 'GET', url: '/api/patientAndSiblings/' + $routeParams['patientID']}).
             success(function (data, status, headers, config) {
-                $scope.patientList = processData(data);
+                $scope.patient = data;
+                $scope.patient2 = data;
+                $scope.patient2.dob = new Date(2013, 9, 22);
             }).
             error(function (data, status, headers, config) {
-                $scope.patientList = [];
+                $scope.patient = {};
+                $scope.patient2 = {};
             });
-    };
-
-    $scope.searchByName = function (name) {
-        $http({method: 'GET', url: '/api/patient/name/' + name}).
-            success(function (data, status, headers, config) {
-                $scope.patientList = processData(data);
-            }).
-            error(function (data, status, headers, config) {
-                $scope.patientList = [];
-            });
-    };
-
-    $scope.changeOrdering = function (field) {
-        $scope.orderByField = field.name;
-        $scope.reverseSort = !$scope.reverseSort;
-    };
-
-    $scope.toggleAllSMS = function () {
-        for (var i = 0; i < $scope.patientList.length; i++) {
-            $scope.patientList[i].sms = !$scope.patientList[i].sms;
-        }
-    };
-
-    $scope.deselectAllSMS = function () {
-        for (var i = 0; i < $scope.patientList.length; i++) {
-            $scope.patientList[i].sms = false;
-        }
-    };
-
-    $scope.sendSMS = function (smsMessage) {
-        var smsList = [];
-        for (var i = 0; i < $scope.patientList.length; i++) {
-            if ($scope.patientList[i].sms === true)
-                smsList.push({id: $scope.patientList[i].id, phone: $scope.patientList[i].phone});
-        }
-        $http.post('/api/patient/sms', {message: smsMessage, smsList: smsList}).
-            success(function (data, status, headers, config) {
-                $scope.smsSuccess = true;
-                $scope.smsFailed = false;
-                $scope.numSMSSuccess = data.numSent;
-            }).
-            error(function (data, status, headers, config) {
-                $scope.smsSuccess = false;
-                $scope.smsFailed = true;
-            });
-    };
-
-    $scope.getSelectedLength = function () {
-        var numSelected = 0;
-        for (var i = 0; i < $scope.patientList.length; i++) {
-            if ($scope.patientList[i].sms === true)
-                numSelected++;
-        }
-        return numSelected;
-    };
-
-    $scope.resetSMSMessage = function () {
+    }]).
+    controller('SearchPatientCtrl', ['$scope', '$http', function($scope, $http) {
+        $scope.patient = {};
+        $scope.patientList = [];
+        $scope.fieldList = [
+            {name: "id", displayName: "ID"},
+            {name: "name", displayName: "Name"},
+            {name: "dob", displayName: "DOB"},
+            {name: "sex", displayName: "Sex"},
+            {name: "phone", displayName: "Phone"}
+        ];
+        $scope.orderByField = 'id';
+        $scope.reverseSort = false;
+        $scope.defaultSMSMessage = "Dear parent,\nWishing your child %name% a very Happy Birthday!\nDr. Mahima\n981129950";
         $scope.smsMessage = $scope.defaultSMSMessage;
-    };
-}
-SearchPatientCtrl.$inject = ['$scope', '$http'];
+        $scope.smsSuccess = false;
+        $scope.numSMSSuccess = 0;
+        $scope.smsFailed = false;
 
-function IndexCtrl() {
-}
-IndexCtrl.$inject = [];
+        var processData = function (pList) {
+            for (var i = 0; i < pList.length; i++) {
+                pList[i].sms = false;
+            }
+            return pList;
+        };
+
+        $scope.searchByID = function (patientId) {
+            $http({method: 'GET', url: '/api/patient/' + patientId}).
+                success(function (data, status, headers, config) {
+                    if (!isEmpty(data))
+                        $scope.patientList = processData([data]);
+                }).
+                error(function (data, status, headers, config) {
+                    $scope.patientList = [];
+                });
+        };
+
+        $scope.searchByDOB = function (dob) {
+            $http({method: 'GET', url: '/api/patient/dob/' + dob}).
+                success(function (data, status, headers, config) {
+                    $scope.patientList = processData(data);
+                }).
+                error(function (data, status, headers, config) {
+                    $scope.patientList = [];
+                });
+        };
+
+        $scope.searchByName = function (name) {
+            $http({method: 'GET', url: '/api/patient/name/' + name}).
+                success(function (data, status, headers, config) {
+                    $scope.patientList = processData(data);
+                }).
+                error(function (data, status, headers, config) {
+                    $scope.patientList = [];
+                });
+        };
+
+        $scope.changeOrdering = function (field) {
+            $scope.orderByField = field.name;
+            $scope.reverseSort = !$scope.reverseSort;
+        };
+
+        $scope.toggleAllSMS = function () {
+            for (var i = 0; i < $scope.patientList.length; i++) {
+                $scope.patientList[i].sms = !$scope.patientList[i].sms;
+            }
+        };
+
+        $scope.deselectAllSMS = function () {
+            for (var i = 0; i < $scope.patientList.length; i++) {
+                $scope.patientList[i].sms = false;
+            }
+        };
+
+        $scope.sendSMS = function (smsMessage) {
+            var smsList = [];
+            for (var i = 0; i < $scope.patientList.length; i++) {
+                if ($scope.patientList[i].sms === true)
+                    smsList.push({id: $scope.patientList[i].id, phone: $scope.patientList[i].phone});
+            }
+            $http.post('/api/patient/sms', {message: smsMessage, smsList: smsList}).
+                success(function (data, status, headers, config) {
+                    $scope.smsSuccess = true;
+                    $scope.smsFailed = false;
+                    $scope.numSMSSuccess = data.numSent;
+                }).
+                error(function (data, status, headers, config) {
+                    $scope.smsSuccess = false;
+                    $scope.smsFailed = true;
+                });
+        };
+
+        $scope.getSelectedLength = function () {
+            var numSelected = 0;
+            for (var i = 0; i < $scope.patientList.length; i++) {
+                if ($scope.patientList[i].sms === true)
+                    numSelected++;
+            }
+            return numSelected;
+        };
+
+        $scope.resetSMSMessage = function () {
+            $scope.smsMessage = $scope.defaultSMSMessage;
+        };
+    }]).
+    controller('IndexCtrl', [function() {
+
+    }]);
